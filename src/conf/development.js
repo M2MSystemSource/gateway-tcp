aa4986ba97bd59f4be5020e4ec9b156b50d53ca7
Jordi Enguídanos <jenguidanos@imasdtecnologia.com>
2018-01-22 09:47:15 +0100

    minor fixes

--- src/conf/development.js
+++ src/conf/development.js
@@ -10,12 +10,6 @@ module.exports = {
     maxAttemptsFails: 10
   },
 
-  redis: {
-    host: '127.0.0.1',
-    port: 6379,
-    password: '40b7a343fb9dfaa54cfd33a92119f7494d9f58be1e99c4e6c96bb06cf8714179'
-  },
-
   watcher: {
     url: 'http://localhost:3010/tracking'
   }
