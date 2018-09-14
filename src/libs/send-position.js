acbd399dd34e34746240540659517b889f5c2315
Jordi Enguídanos <jenguidanos@imasdtecnologia.com>
2018-09-14 08:18:31 +0200

    arrg

--- src/libs/send-position.js
+++ src/libs/send-position.js
@@ -48,6 +48,8 @@ module.exports = (app) => {
         // Se debe enviar notificación a algún canal
       }
 
+      console.log('send-position', position)
+
       const args = {
         data: position,
         headers: {'Content-Type': 'application/json'}
